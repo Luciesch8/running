a18d9f0c6bfc4786f15eba6612418fdb821b52f8
Lucie <luciesch8@gmail.com>
2023-02-19 18:33:41 +0100

    Initial Commit

---
+++ Running/RunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningApp.swift
+//  Running
+//
+//  Created by Ah lucie nous gênes 🍄 on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
